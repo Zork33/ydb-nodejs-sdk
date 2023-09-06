5d3676a84c2819982bdc11332be0625c91c93c8d
Alexey Zorkaltsev <AZorkaltsev@Yandex-Team.ru>
2023-09-06 17:15:08 +0300

    chore: refactoring: table.ts split to few files

---
+++ src/table/index.ts
@@ -0,0 +1,8 @@
+// package internal
+// export * from './ITableClientSettings';
+// export * from './sessionEvent';
+
+export * from './session';
+export * from './sessionPool';
+export * from './tableClient';
+export * from './ydb';
